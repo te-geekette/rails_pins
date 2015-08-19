150ada66cd128b4f7c17353dfa7892ba89894fb0
Tina Egolf <tinaegolf@gmail.com>
2015-08-19 22:49:26 +0200

    First pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Pins::Application.routes.draw do
+  root 'pages#home'
+  get 'about' => 'pages#about'    #creates about_path that can be linked to
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
